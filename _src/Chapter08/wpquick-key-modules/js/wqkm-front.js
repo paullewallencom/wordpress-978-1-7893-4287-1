bb5431929f98df5db41ab8ce1e1605376721c53a
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-25 18:02:34 -0800

    Init
    Init commit.

---
+++ _src/Chapter08/wpquick-key-modules/js/wqkm-front.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+          
+  $('#banner-fade').bjqs({
+    animtype      : 'slide',
+    height        : WQKMFront.height,
+    width         : WQKMFront.width,
+    responsive    : true,
+    randomstart   : true,
+	showmarkers   : false,
+  });
+ 
+});
